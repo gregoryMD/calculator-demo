e33f30d2c75f59cd82ee0724a55ca0fe4cb3ef08
gregoryMD <gdiehl265@gmail.com>
2023-05-02 11:02:04 -0400

    linting

--- src/App.test.js
+++ src/App.test.js
@@ -3,23 +3,23 @@ import App from "./App";
 
 describe("App component has all button elements", () => {
   it('should contain an "=" button with an id="equals"', () => {
-    const { getByTestId } = render(<App />);
-    const equalsButton = getByTestId("equals");
+    render(<App />);
+    const equalsButton = screen.getByTestId("equals");
     expect(equalsButton).toBeInTheDocument();
   });
 
   it("should contain 10 number buttons with the corresponding IDs", () => {
-    const { getByTestId } = render(<App />);
-    const zero = getByTestId("zero");
-    const one = getByTestId("one");
-    const two = getByTestId("two");
-    const three = getByTestId("three");
-    const four = getByTestId("four");
-    const five = getByTestId("five");
-    const six = getByTestId("six");
-    const seven = getByTestId("seven");
-    const eight = getByTestId("eight");
-    const nine = getByTestId("nine");
+    render(<App />);
+    const zero = screen.getByTestId("zero");
+    const one = screen.getByTestId("one");
+    const two = screen.getByTestId("two");
+    const three = screen.getByTestId("three");
+    const four = screen.getByTestId("four");
+    const five = screen.getByTestId("five");
+    const six = screen.getByTestId("six");
+    const seven = screen.getByTestId("seven");
+    const eight = screen.getByTestId("eight");
+    const nine = screen.getByTestId("nine");
     expect(zero).toBeInTheDocument();
     expect(one).toBeInTheDocument();
     expect(two).toBeInTheDocument();
@@ -33,11 +33,11 @@ describe("App component has all button elements", () => {
   });
 
   it("should contain 4 operator buttons with the corresponding IDs", () => {
-    const { getByTestId } = render(<App />);
-    const add = getByTestId("add");
-    const subtract = getByTestId("subtract");
-    const multiply = getByTestId("multiply");
-    const divide = getByTestId("divide");
+    render(<App />);
+    const add = screen.getByTestId("add");
+    const subtract = screen.getByTestId("subtract");
+    const multiply = screen.getByTestId("multiply");
+    const divide = screen.getByTestId("divide");
     expect(add).toBeInTheDocument();
     expect(subtract).toBeInTheDocument();
     expect(multiply).toBeInTheDocument();
@@ -45,37 +45,37 @@ describe("App component has all button elements", () => {
   });
 
   it('should contain a "." button with an id="decimal"', () => {
-    const { getByTestId } = render(<App />);
-    const decimalButton = getByTestId("decimal");
+    render(<App />);
+    const decimalButton = screen.getByTestId("decimal");
     expect(decimalButton).toBeInTheDocument();
   });
 
   it('should contain a "clear" button with an id="clear"', () => {
-    const { getByTestId } = render(<App />);
-    const clearButton = getByTestId("clear");
+    render(<App />);
+    const clearButton = screen.getByTestId("clear");
     expect(clearButton).toBeInTheDocument();
   });
 
   it("should contain a a text element to display values", () => {
-    const { getByTestId } = render(<App />);
-    const display = getByTestId("display");
+    render(<App />);
+    const display = screen.getByTestId("display");
     expect(display).toBeInTheDocument();
   });
 });
 
 test("Clear button should reset calculator to initial state", () => {
-  const { getByTestId } = render(<App />);
-  const displayElement = getByTestId("display");
-  const clearButton = getByTestId("clear");
+  render(<App />);
+  const displayElement = screen.getByTestId("display");
+  const clearButton = screen.getByTestId("clear");
   fireEvent.click(clearButton);
   expect(displayElement.textContent).toEqual("0");
 });
 
 describe("display element should display user input as it is entered", () => {
   it("should display the first nuber entered", () => {
-    const { getByTestId } = render(<App />);
-    const displayElement = getByTestId("display");
-    const six = getByTestId("six");
+    render(<App />);
+    const displayElement = screen.getByTestId("display");
+    const six = screen.getByTestId("six");
     fireEvent.click(six);
     fireEvent.click(six);
     fireEvent.click(six);
@@ -83,21 +83,21 @@ describe("display element should display user input as it is entered", () => {
   });
 
   it("should display the operator when pressed", () => {
-    const { getByTestId } = render(<App />);
-    const displayElement = getByTestId("display");
-    const six = getByTestId("six");
-    const plus = getByTestId("add");
+    render(<App />);
+    const displayElement = screen.getByTestId("display");
+    const six = screen.getByTestId("six");
+    const plus = screen.getByTestId("add");
     fireEvent.click(six);
     fireEvent.click(plus);
     expect(displayElement.textContent).toEqual("+");
   });
 
   it("should display the second number when entered", () => {
-    const { getByTestId } = render(<App />);
-    const displayElement = getByTestId("display");
-    const six = getByTestId("six");
-    const plus = getByTestId("add");
-    const three = getByTestId("three");
+    render(<App />);
+    const displayElement = screen.getByTestId("display");
+    const six = screen.getByTestId("six");
+    const plus = screen.getByTestId("add");
+    const three = screen.getByTestId("three");
     fireEvent.click(six);
     fireEvent.click(plus);
     fireEvent.click(three);
@@ -106,12 +106,12 @@ describe("display element should display user input as it is entered", () => {
 });
 
 test("the correct answer should be displayed when the equal button is pressed", () => {
-  const { getByTestId } = render(<App />);
-  const displayElement = getByTestId("display");
-  const six = getByTestId("six");
-  const plus = getByTestId("add");
-  const three = getByTestId("three");
-  const equals = getByTestId("equals");
+  render(<App />);
+  const displayElement = screen.getByTestId("display");
+  const six = screen.getByTestId("six");
+  const plus = screen.getByTestId("add");
+  const three = screen.getByTestId("three");
+  const equals = screen.getByTestId("equals");
   fireEvent.click(six);
   fireEvent.click(plus);
   fireEvent.click(three);
